42b3f21177c22f46709b557e5ebc0a0f83b66b84
Ren <ren.t21@icloud.com>
2022-02-07 17:21:08 +0900

    236

---
+++ TodoApp/TodoListViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by 高橋蓮 on 2022/02/07.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    var itemArray = ["Find MIke", "Buy Egg", "Go School"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    //MARK: -TableView tadaSource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    //MARK: -TableView Delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    //MARK: -Add new item
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "新しいTODOリストを追加します", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "追加", style: .default) { action in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "追加するリストを入力"
+            textField = alertTextField
+            
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+}
+
+

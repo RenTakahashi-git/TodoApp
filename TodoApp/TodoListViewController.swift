0cf165380cfe8fb5c244df34031d5ef48dae656c
Ren <ren.t21@icloud.com>
2022-02-11 21:48:14 +0900

    258

--- TodoApp/TodoListViewController.swift
+++ TodoApp/TodoListViewController.swift
@@ -93,13 +93,13 @@ class TodoListViewController: UITableViewController {
     
     
     
-    func loadItems() {
-        let request: NSFetchRequest<Item> = Item.fetchRequest()
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
         do {
             itemArray = try context.fetch(request)
         } catch {
             print("Error in fetching request")
         }
+        tableView.reloadData()
     }
     
 }
@@ -107,16 +107,18 @@ class TodoListViewController: UITableViewController {
 extension TodoListViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         let request: NSFetchRequest<Item> = Item.fetchRequest()
-        request.predicate = NSPredicate(format: "title contains[cd] %@", searchBar.text!)
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
         request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
-        
-        do {
-            itemArray = try context.fetch(request)
-        } catch {
-            print("Error in fetching request")
+        loadItems(with: request)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
         }
-        tableView.reloadData()
-        
     }
 }
 
